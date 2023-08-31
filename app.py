220d584be0aa4419e0103662edef11ca00f0cfaa
vinsky001 <ekibet544@gmail.com>
2023-08-31 19:40:52 +0300

    Created database table

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+#Connects our app file to the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+#creates an instance of the database
+db = SQLAlchemy(app)
+
+#Let's create database table
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    #Username to have a maximum of 20 characters
+    username = db.Column(db.String(20), nullable=False)
+    #password to have a max of 80 characters
+    password = db.Column(db.String(80), nullable=False)
+                         
+                         
 
 app.route('/')
 def home():
