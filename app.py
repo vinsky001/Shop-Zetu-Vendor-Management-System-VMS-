540d61d507f8bf13aed1c0d4e7c515f22569671e
vinsky001 <ekibet544@gmail.com>
2023-08-31 15:35:55 +0300

    Database config

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 app.route('/')
 def home():
