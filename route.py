4cbf6f0cbb42054f4eef97c608730b6a218ba803
vinsky001 <ekibet544@gmail.com>
2023-08-31 23:24:15 +0300

    Added username and password validators

--- route.py
+++ route.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
 
 
 app = Flask(__name__)
@@ -15,11 +20,28 @@ db = SQLAlchemy(app)
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     #Username to have a maximum of 20 characters
-    username = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
     #password to have a max of 80 characters
     password = db.Column(db.String(80), nullable=False)
-                         
-                         
+ 
+ #This class inherits from FlaskForm                         
+class Join_as_vendor(FlaskForm):
+    username = String(validators=[
+                    InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "username"})
+    
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    
+    submit = SubmitField("Register")
+    
+    #Validates username inputs by checking the database
+    def  validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        #if there is a similar username:
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')                    
 
 app.route('/')
 def home():
