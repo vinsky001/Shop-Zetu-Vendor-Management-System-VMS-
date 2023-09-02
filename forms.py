d17ceb7bea7d7791818e9bee844e5bf68cad4578
vinsky001 <ekibet544@gmail.com>
2023-09-02 20:22:15 +0300

    Added a function that checks if a username exits in the database and handles errors that comes with the same

--- forms.py
+++ forms.py
@@ -4,6 +4,15 @@ from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 #User/vendor registration
 class Join_as_Vendor(FlaskForm):
+    #Validates username inputs by checking the database
+    def  validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        #if there is a similar username:
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+    
     username = StringField(label='Username', validators=[Length(min=5, max=15), DataRequired()])
     email_address = StringField(label='Email Address', validators=[Email(), DataRequired()])
     brand_name = StringField(label='Brand Name', validators=[Length(max=15), DataRequired()])
