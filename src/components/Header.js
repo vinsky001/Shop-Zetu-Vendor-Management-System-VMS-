b3e04e63588181ba37f87333a2bafdd98ba2e3ae
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-09-12 00:27:32 +0100

    Changed 'class' to 'className'.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 export default function Header() {
 
   return (
-    <header class="header flex-grow-0">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand text-bold" href="./">
+    <header className="header flex-grow-0">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <a className="navbar-brand text-bold" href="./">
             ShopZetu
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarScroll"
@@ -16,44 +16,44 @@ export default function Header() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarScroll">
+          <div className="collapse navbar-collapse" id="navbarScroll">
             <ul
-              class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll mx-auto"
+              className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll mx-auto"
             >
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="./">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="./">
                   Home
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./">
+              <li className="nav-item">
+                <a className="nav-link" href="./">
                   About
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./">
+              <li className="nav-item">
+                <a className="nav-link" href="./">
                   Brands
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./">
-                  Pricing
+              <li className="nav-item">
+                <a className="nav-link" href="./">
+                  My Dashboard
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./">
+              <li className="nav-item">
+                <a className="nav-link" href="./">
                   Join as Vendor
                 </a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./">
+              <li className="nav-item">
+                <a className="nav-link" href="./">
                   Contact Us
                 </a>
               </li>
             </ul>
-            <button class="btn btn-default text-bold" type="submit">
+            <button className="btn btn-default text-bold" type="submit">
               Sign in
             </button>
           </div>
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,48 +1,49 @@
 import junior_reis from "../assets/images/junior-reis.jpg";
+import sparkles from "../assets/svgs/sparkles.svg";
 
 export default function Jumbotron() {
   return (
-    <div class="jumbotron container flex-grow-1 h-100">
-      <div class="row h-100 align-items-center">
-        <div class="col-12 col-md-6">
-          <div class="wrap">
-            <i class="fas fa-star-of-life blue-text"></i>
+    <div className="jumbotron container flex-grow-1 h-100">
+      <div className="row h-100 align-items-center" style={{ marginTop: "100px" }}>
+        <div className="col-12 col-md-6">
+          <div className="wrap">
+            <img className="sz-icon" src={sparkles} alt="Sparkles" />
             <h1>
-              Streamline <span class="blue-text">Management</span>
+              Streamline <span className="blue-text">Management</span>
               <br />
-              for&nbsp;<span class="blue-text">Vendors</span> in the Fashion
+              for&nbsp;<span className="blue-text">Vendors</span> in the Fashion
               <br />
               Industry
             </h1>
-            <p class="text-justify">
+            <p className="">
               Introducing ShopZetu's Vendor Management System&nbsp;! An
               essential tool for every vendor to help ease workflow.
             </p>
-            <form class="row g-3 mx-auto">
-              <div class="col-8 col-md-9">
-                <label for="email" class="visually-hidden">
+            <form className="row g-3 mx-auto">
+              <div className="col-8 col-md-8">
+                <label for="email" className="visually-hidden">
                   Email
                 </label>
                 <input
                   type="email"
-                  class="form-control email-box"
+                  className="form-control email-box"
                   id="email"
                   placeholder="enter email"
                   autofocus
                 />
               </div>
-              <div class="col-4 col-md-3">
-                <button type="submit" class="btn btn-default mb-3">
+              <div className="col-4 col-md-4">
+                <button type="submit" className="btn btn-default mb-3">
                   Contact us
                 </button>
               </div>
             </form>
           </div>
         </div>
-        <div class="col-12 col-md-6 d-flex justify-content-center h-100">
-          <i class="fas fa-star-of-life blue-text"></i>
-          <img alt="Junior Reis" class="img-fluid sz-img" src={junior_reis} />
-          <i class="flex-start fas fa-star-of-life blue-text"></i>
+        <div className="col-12 col-md-6 d-flex justify-content-center h-100">
+          <img className="sz-icon" src={sparkles} alt="Sparkles" />
+          <img alt="Junior Reis" className="img-fluid sz-img" src={junior_reis} />
+          <img className="sz-icon" src={sparkles} alt="Sparkles" />
         </div>
       </div>
     </div>
