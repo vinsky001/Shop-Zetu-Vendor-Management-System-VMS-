784ea94ab56460a1f17943702a2beb15cd218142
Sunday Oruwhone <sundayoruwhone@gmail.com>
2023-09-12 10:04:45 +0100

    Added: signin/signup box.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+// import SigninForm from "./signinform";
+
 export default function Header() {
   return (
     <header className="header flex-grow-0">
@@ -18,9 +20,7 @@ export default function Header() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarScroll">
-            <ul
-              className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll mx-auto"
-            >
+            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll mx-auto">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="./">
                   Home
@@ -52,9 +52,40 @@ export default function Header() {
                 </a>
               </li>
             </ul>
-            <button className="btn btn-default text-bold" type="submit">
+            <button
+              className="btn btn-default text-bold"
+              type="button"
+              data-bs-toggle="modal"
+              data-bs-target="#signinPopup"
+            >
               Sign in
             </button>
+            {/* Signin Popup */}
+            <div
+              className="modal fade"
+              id="signinPopup"
+              tabindex="-1"
+              aria-labelledby="signinPopupLabel"
+              aria-hidden="true"
+            >
+              <div className="modal-dialog modal-dialog-centered modal-dialog modal-xl">
+                <div className="modal-content br-0">
+                  <div className="modal-header b-0">
+                    <button
+                      type="button"
+                      className="btn-close px-1 py-0"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div className="modal-body">
+                    <p className="text-center m-0">Signin & Signup Form</p>
+                    {/* <SigninForm /> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Signin Popup */}
           </div>
         </div>
       </nav>
