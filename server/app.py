f33a1984dec04730869502b4abbce6260996acf2
Houdini Papi <ochiengcliff.co@gmail.com>
2023-09-12 11:15:23 +0300

    User authentication

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS, cross_origin
+from flask_session import Session
 from config import ApplicationConfig
 from models import db, User
 
@@ -7,10 +9,23 @@ app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
+CORS(app, supports_credentials=True)
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+@app.route("/@me")
+def get_current_user():
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({"id": user.id, "email": user.email})
 
 
 @app.route("/register", methods=["POST"])
@@ -28,7 +43,32 @@ def register_user():
     db.session.add(new_user)
     db.session.commit()
 
+    session["user_id"] = new_user.id
+
     return jsonify({"id": new_user.id, "email": new_user.email})
+
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+
+    session["user_id"] = user.id
+
+    return jsonify({"id": user.id, "email": user.email})
+
+@app.route("/logout", methods=["POST"])
+def logout_user():
+    session.pop("user_id")
+    return "200"
 
 
 if __name__ == "__main__":
--- server/config.py
+++ server/config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import redis
 
 load_dotenv()
 
@@ -9,3 +10,8 @@ class ApplicationConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
+
+    SESSION_TYPE = "redis"
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
