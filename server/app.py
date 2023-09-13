982bf412239356e7f5d845ade19b7c46bd30bf12
Houdini Papi <ochiengcliff.co@gmail.com>
2023-09-13 13:26:28 +0300

    Backend functionality

--- server/app.py
+++ server/app.py
@@ -3,13 +3,26 @@ from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
 from flask_session import Session
 from config import ApplicationConfig
-from models import db, User, Message
+from models import db, User
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
+# Connects our app file to the database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./db.sqlite"
+app.config["SECRET_KEY"] = "18f3e4d01227bda3eabd490c"
+app.config["MAIL_SERVER"] = "smtp.gmail.com."
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USERNAME"] = "ekibet544@gmail.com"
+app.config["MAIL_PASSWORD"] = "99icloud"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+
+
 bcrypt = Bcrypt(app)
 CORS(app, supports_credentials=True)
+mail = Mail(app)
 server_session = Session(app)
 db.init_app(app)
 
@@ -17,6 +30,7 @@ with app.app_context():
     db.create_all()
 
 
+# Getting current user (Home page)
 @app.route("/@me")
 def get_current_user():
     user_id = session.get("user_id")
@@ -28,6 +42,7 @@ def get_current_user():
     return jsonify({"id": user.id, "email": user.email})
 
 
+# Registering a new user
 @app.route("/register", methods=["POST"])
 def register_user():
     email = request.json["email"]
@@ -48,6 +63,7 @@ def register_user():
     return jsonify({"id": new_user.id, "email": new_user.email})
 
 
+# Logging in an existing user
 @app.route("/login", methods=["POST"])
 def login_user():
     email = request.json["email"]
@@ -66,23 +82,48 @@ def login_user():
     return jsonify({"id": user.id, "email": user.email})
 
 
-@app.route("/send_message", methods=["POST"])
-def send_message():
-    user_id = session.get("user_id")
+# @app.route("/send_message", methods=["POST"])
+# def send_message():
+#     user_id = session.get("user_id")
 
-    if not user_id:
-        return jsonify({"error": "Unauthorized"}), 401
-    
-    message_text = request.json["message_text"]
-    user_email = request.json["user_email"]
+#     if not user_id:
+#         return jsonify({"error": "Unauthorized"}), 401
 
-    # Create a new message and save it to the database
-    new_message = Message(sender_id=user_id, message=message_text)
-    db.session.add(new_message)
-    db.session.commit()
+#     message_text = request.json["message_text"]
+#     user_email = request.json["user_email"]
 
-    return jsonify({"message": "essage sent successfully", "user_email": user_email, "message_text": message_text})
+#     # Create a new message and save it to the database
+#     new_message = Message(sender_id=user_id, message=message_text)
+#     db.session.add(new_message)
+#     db.session.commit()
 
+#     return jsonify({"message": "essage sent successfully", "user_email": user_email, "message_text": message_text})
+
+
+# Contact form and send emails with Flask for support or feedback
+@app.route("/contact", methods=["POST"])
+def contact():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+    message = request.form.get("message")
+
+    subject = f"Mail from {name}"
+    body = f"Name: {name}\nE-mail: {email}\nPhone: {phone}\n\n\n{message}"
+    sender = app.config["MAIL_USERNAME"]
+    recipients = ["ekibet544@gmail.com"]
+
+    # Create an email message
+    msg = Message(subject=subject, body=body, sender=sender, recipients=recipients)
+
+    try:
+        mail.send(msg)
+        return jsonify({"message": "Email sent successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+# Logging out a user
 @app.route("/logout", methods=["POST"])
 def logout_user():
     session.pop("user_id")
--- server/models.py
+++ server/models.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from uuid import uuid4
-from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -14,14 +13,3 @@ class User(db.Model):
     id = db.Column(db.String(32), primary_key=True, unique=True, default=get_uuid)
     email = db.Column(db.String(345), unique=True)
     password = db.Column(db.Text, nullable=False)
-
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    message = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __init__(self, sender_id, message):
-        self.sender_id = sender_id
-        self.message = message
